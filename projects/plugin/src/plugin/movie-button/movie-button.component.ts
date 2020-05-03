19ec51c2f618be4a20b1495bbaa7e29be070b3b1
Shaheen7m <shaheen86@gmail.com>
2020-05-03 12:36:54 +0100

    Work around for missing ratings

--- projects/plugin/src/plugin/movie-button/movie-button.component.ts
+++ projects/plugin/src/plugin/movie-button/movie-button.component.ts
@@ -31,13 +31,50 @@ export class MovieButtonComponent extends MovieDetailBaseComponent {
             value: res.Value
           });
         });
-        //this.transform(this.ratings);
+        this.ratings = this.transform(this.ratings);
       },
       (error) => {
         console.log('Error: ', error);
         //this.response = null;
       }
     );
+  }
+
+  transform(ratings: Ratings[]) {
+    let hasIMDb: boolean = false;
+    let hasRT: boolean = false;
+    let hasMeatCritic: boolean = false;
+
+    ratings.forEach((rating) => {
+      if (rating.source == 'Internet Movie Database') {
+        hasIMDb = true;
+      } else if (rating.source == 'Rotten Tomatoes') {
+        hasRT = true;
+      } else if (rating.source == 'Metacritic') {
+        hasMeatCritic = true;
+      }
+    });
+
+    if (!hasIMDb) {
+      ratings.push({
+        source: 'Internet Movie Database',
+        value: '--'
+      });
+    }
+    if (!hasRT) {
+      ratings.push({
+        source: 'Rotten Tomatoes',
+        value: '--'
+      });
+    }
+    if (!hasMeatCritic) {
+      ratings.push({
+        source: 'Metacritic',
+        value: '--'
+      });
+    }
+
+    return ratings;
   }
 
   setMovie(movie: Movie): any {
